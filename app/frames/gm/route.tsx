c048114965806f50296e5f0dcfb452df3b045203
defispartan <defispartan@gmail.com>
2024-07-24 22:33:38 -0500

    gm

--- app/frames/gm/route.tsx
+++ app/frames/gm/route.tsx
@@ -1,15 +1,11 @@
 import { Button } from "frames.js/next";
-import { frames } from "./frames";
+import { frames } from "../frames";
 export const runtime = "nodejs";
 
 const handleRequest = frames(async (ctx) => {
   return {
-    image: "https://i.ibb.co/SmLYSKr/openframesupport.jpg",
-    buttons: [
-      <Button action="post" target="/gm">
-        gm
-      </Button>,
-    ],
+    image: "https://i.ibb.co/f8tvk88/gm.png",
+    buttons: [],
   };
 });
 
